8f9703c8fc1051a7e6bb278ab0716aee3e25bb7d
Thong Phan <phanthong2k000@gmail.com>
2021-04-26 10:42:12 +0700

    tuan1_android

---
+++ 18043151_PhanVanThong_Android_Tuan1/app/src/main/java/com/example/a18043151_phanvanthong_android_tuan1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a18043151_phanvanthong_android_tuan1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ 18043151_PhanVanThong_Android_Tuan1/app/src/main/java/com/example/a18043151_phanvanthong_android_tuan1/Tuan1_Bai5.java
@@ -0,0 +1,14 @@
+package com.example.a18043151_phanvanthong_android_tuan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tuan1_Bai5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tuan1__bai5);
+    }
+}
