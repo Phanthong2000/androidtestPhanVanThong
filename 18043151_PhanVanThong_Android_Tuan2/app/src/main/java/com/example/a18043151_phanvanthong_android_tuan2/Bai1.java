28a54a90cb5a4da003f4919529a29a3691ff7938
Thong Phan <phanthong2k000@gmail.com>
2021-04-26 11:05:33 +0700

    tuan2_android

---
+++ 18043151_PhanVanThong_Android_Tuan2/app/src/main/java/com/example/a18043151_phanvanthong_android_tuan2/Bai1.java
@@ -0,0 +1,14 @@
+package com.example.a18043151_phanvanthong_android_tuan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bai1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai1);
+    }
+}
