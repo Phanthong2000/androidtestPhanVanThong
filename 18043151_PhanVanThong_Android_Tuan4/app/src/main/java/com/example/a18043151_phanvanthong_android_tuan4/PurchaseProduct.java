7fc60d852e6aa7920f42b637024bdb8a5029425d
Thong Phan <phanthong2k000@gmail.com>
2021-04-27 14:59:46 +0700

    tuan3_android

---
+++ 18043151_PhanVanThong_Android_Tuan4/app/src/main/java/com/example/a18043151_phanvanthong_android_tuan4/PurchaseProduct.java
@@ -0,0 +1,6 @@
+package com.example.a18043151_phanvanthong_android_tuan4;
+
+public interface PurchaseProduct {
+    public void addProduct(int quantity, double donGia);
+    public void subProduct(int quantity, double donGia);
+}
